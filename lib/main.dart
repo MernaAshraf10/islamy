87927f63182b215647ee8fcaff2eebbfae276592
merna <mernaashraf910@gmail.com>
2021-10-29 22:55:05 +0200

    initial project added empty home page

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:islamy/HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      routes: {HomePage.routename: (buildContext) => HomePage()},
+      initialRoute: HomePage.routename,
+    );
+  }
+}
